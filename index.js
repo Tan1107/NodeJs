f4c0e9776daecc841aee1f2fa8a97626eb9f4d4a
vatakudo <tannguyen1107.bku@gmail.com>
2020-07-22 00:30:25 +0700

    commit start

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var app = express();
+
+var port = 3000
+
+app.get('/', function(request, response){
+    response.send("<h1>hello may</h1>")
+})
+app.get('/user', function(request, response){
+    response.send("<h1>hello may thang user</h1>")
+})
+
+//GET, POST, REQUEST
+
+app.listen(port, function(){
+    console.log("start server on port 3000")
+})
